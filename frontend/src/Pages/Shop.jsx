befc858691cb3d175337da84f03315ab3e8a3c9a
Akaz Jaya <akazjaya096@gmail.com>
2024-06-27 14:03:36 +0530

    first commit

---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../Component/Hero/Hero";
+
+const Shop = () =>{
+
+    return(
+
+        <div>
+            <Hero/>
+        </div>
+    )
+}
+
+export default Shop;
